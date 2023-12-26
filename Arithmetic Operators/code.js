6e7451f55ce5222c3a36efd2487036dc001884db
dev-alihasan <infomeorsendmehelp@protonmail.com>
2023-12-26 22:42:01 +0600

    artihmetic numbers

---
+++ Arithmetic Operators/code.js
@@ -0,0 +1,11 @@
+/**Arithmetic Operators */
+
+let addition = 1 + 1; /**2 */
+
+let multiplication = 2 * 2; /**4 */
+
+let division = 4 / 2; /**2 */
+
+let subtraction = 4 - 2; /**2 */
+
+let modulo = 4 % 2; /**0 */
