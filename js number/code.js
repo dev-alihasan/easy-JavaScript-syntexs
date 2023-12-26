b7e19bb7b25a5c1c9819a786d2e0841f3e52633f
dev-alihasan <infomeorsendmehelp@protonmail.com>
2023-12-26 21:56:23 +0600

    variable

--- js number/code.js
+++ js number/code.js
@@ -3,6 +3,6 @@ let mobilePrice = 10;
 
 let total = pcPrice + mobilePrice;
 
-/**javascript number we dont use  single quotation marks '' or double quotation marks "" to declare numbers in javaScript */
+/**we dont use  single quotation marks '' or double quotation marks "" to declare numbers in javaScript */
 
 console.log(total);
---
+++ variables in JavaScript/code.js
@@ -0,0 +1,10 @@
+/**  variables is  like a box or a container, where u can store information/ dta or things. like persons name
+ * number, arry
+ */
+
+var box = "ben 10 toy";
+let age = 10;
+/*you can change what's inside the box whenever you want. It's like having a container that you can use and update as needed.*/
+
+const gender = "male";
+/* u can't change once u put something in it conts is like a special container */
