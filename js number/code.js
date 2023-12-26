000988db7d3ec6f0667e415d7169c79d2ac94e0b
dev-alihasan <infomeorsendmehelp@protonmail.com>
2023-12-26 20:42:31 +0600

    go boy

---
+++ js number/code.js
@@ -0,0 +1,8 @@
+let pcPrice = 10;
+let mobilePrice = 10;
+
+let total = pcPrice + mobilePrice;
+
+/**javascript number we dont use  single quotation marks '' or double quotation marks "" to declare numbers in javaScript */
+
+console.log(total);
