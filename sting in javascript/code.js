8d987b82614b1994b08b021887c75e3ee905cbbf
dev-alihasan <infomeorsendmehelp@protonmail.com>
2023-12-26 22:22:29 +0600

    string js

---
+++ sting in javascript/code.js
@@ -0,0 +1,6 @@
+/**Strings are useful for holding data that can be represented in text form. u can declare sting using single qoutetion mark '' or  duble qoutation mark  " "
+ *
+ */
+
+let box1 = "this is a string";
+let box2 = "this is aslo a string";
