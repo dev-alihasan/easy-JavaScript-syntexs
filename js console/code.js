2b413f3fd507b922f038a92a1a2caef4a48fcfa1
dev-alihasan <infomeorsendmehelp@protonmail.com>
2023-12-26 20:36:02 +0600

    go boy

--- js console/code.js
+++ js console/code.js
@@ -1,3 +1,13 @@
 console.log('web dev arnob');
 
 console.log("web dev arnob")
+
+console.log(4+4); /*addition*/
+
+console.log(4*4); /*multiplication*/ 
+
+console.log(4-2) /* subtraction*/
+
+console.log(4/2) /* division*/
+
+/*u can do other many things with console.log()*/
