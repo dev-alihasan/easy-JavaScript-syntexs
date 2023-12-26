4f60d5a36e32f9d79d5cfd85460bd19af73cbae9
dev-alihasan <infomeorsendmehelp@protonmail.com>
2023-12-26 23:10:50 +0600

    Subtract and Assign

---
+++ assignment operator/code.js
@@ -0,0 +1,33 @@
+/*assignment operator is like giving instructions to change or set the value of something. */
+
+/*equal Sign =
+make this equal to that.
+*/
+let toyCar = 10
+
+/*it mean u have 10 toy car.*/
+/*---------------------------------- */
+
+
+/*add and Assign +=*/
+let toyCar = 10 
+toyCar += 2 
+/*add this to what's already there, it's mean u have 10 car and now using this +=2 
+now u have 10 toy car
+ */
+
+
+
+/*---------------------------------- */
+// Subtract and Assign -= this sign mean take this away from what's already there.
+
+let toyCar = 12
+
+toyCar -= 2 
+
+/*after using -= this now u've again 10 toy car */
+
+
+/*---------------------------------- */
+
+
